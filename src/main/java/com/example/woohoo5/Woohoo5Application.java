3e84fe0c24d3619145d118721122941a776865cf
Keith Lockey <lockey.keith@gmail.com>
2023-07-05 10:53:56 -0400

    Initial commit

---
+++ src/main/java/com/example/woohoo5/Woohoo5Application.java
@@ -0,0 +1,13 @@
+package com.example.woohoo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Woohoo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Woohoo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/woohoo5/Woohoo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woohoo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Woohoo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
